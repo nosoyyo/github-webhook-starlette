6edbcaafedc7e92ddfa5952ffd173e493e0c6105
Paul Carino <paulcarinocrafts@gmail.com>
2019-01-19 15:37:26 +0800

    deal with pid

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # @falcon.ny
 import os
 import json
+import redis
 import uvicorn
 from starlette.applications import Starlette
 from starlette.endpoints import HTTPEndpoint
@@ -10,6 +11,11 @@ from config import Conf
 
 
 app = Starlette(debug=True)
+cpool = redis.ConnectionPool(host='localhost',
+                             port=6379,
+                             decode_responses=True,
+                             db=0)
+r = redis.Redis(connection_pool=cpool)
 
 
 @app.route("/github_webhook")
@@ -19,6 +25,14 @@ class WebHook(HTTPEndpoint):
     On `push` event, do a shell script which should be set in config.py
 
     '''
+
+    def __init__(self):
+        self.repo_id = Conf.projects['gws']
+        self.pid = os.getpid()
+        r.set(self.repo_id, self.pid)
+
+    def __del__(self):
+        r.delete(self.repo_id)
 
     async def get(self, request):
         resp = {'ok': True}
@@ -31,8 +45,10 @@ class WebHook(HTTPEndpoint):
         '''
         event = request.headers['X-GitHub-Event']
         form = await request.form()
+        print(form)
         payload = json.loads(form['payload'])
         repo_id = payload['repository']['id']
+        pid = r.get(repo_id)
         _sh = Conf.projects[repo_id]['shell_script']
         _dir = Conf.projects[repo_id]['dir']
         result = False
@@ -41,7 +57,8 @@ class WebHook(HTTPEndpoint):
             try:
                 # normally `_sh` return `0`, which could be considered as False
                 os.chdir(_dir)
-                result = not os.system(f'bash {_sh}')
+                # always accept a `pid` param for killing the old one
+                result = not os.system(f'bash {_sh} {pid}')
                 if result:
                     message = f'successfully run {_sh}'
                 else:
