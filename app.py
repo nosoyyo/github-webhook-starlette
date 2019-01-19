d8012922c182cd8b155a90813a8b78f9b57ea50a
Paul Carino <paulcarinocrafts@gmail.com>
2019-01-19 18:18:03 +0800

    mostly done everything

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 import os
 import json
 import uvicorn
+from uvicorn.main import run, get_logger
+from uvicorn.reloaders.statreload import StatReload
 from starlette.applications import Starlette
 from starlette.endpoints import HTTPEndpoint
 from starlette.responses import JSONResponse
-from uvicorn.main import run, get_logger
-from uvicorn.reloaders.statreload import StatReload
 
 from config import Conf
 
@@ -21,7 +21,6 @@ class WebHook(HTTPEndpoint):
     On `push` event, do a shell script which should be set in config.py
 
     '''
-    repo_id = Conf.projects['gws']
 
     async def get(self, request):
         resp = {'ok': True, 'message': 'you got this'}
@@ -29,10 +28,9 @@ class WebHook(HTTPEndpoint):
 
     async def post(self, request):
         '''
-        :var repo_id: <int> here we should pay attn. on this exception
+        :var repo_id: <int>
         '''
-        event = request.headers['X-GitHub-Event']
-        self.event = event
+        self.event = request.headers['X-GitHub-Event']
         form = await request.form()
         print(form)
         payload = json.loads(form['payload'])
@@ -41,24 +39,25 @@ class WebHook(HTTPEndpoint):
         _dir = Conf.projects[repo_id]['dir']
         result = False
 
-        if event == 'push':
+        if self.event == 'push':
             try:
-                # normally `_sh` return `0`, which could be considered as False
                 os.chdir(_dir)
-                # always accept a `pid` param for killing the old one
-                result = not os.system(f'bash {_sh}')
-                if result:
-                    message = f'successfully run {_sh}'
-                else:
-                    message = f'failed run {_sh}'
+                os.system('git pull')
+                message = 'pulled from github.'
+                if _sh:
+                    result = not os.system(f'bash {_sh}')
+                    if result:
+                        message = f'successfully run {_sh}'
+                    else:
+                        message = f'failed run {_sh}'
             except Exception as e:
                 message = e
         else:
-            message = await self.getPingMessage(payload)
+            message = await self.echo(payload)
         resp = {'done': result, 'message': message}
         return JSONResponse(resp)
 
-    async def getPingMessage(self, payload):
+    async def echo(self, payload):
         return f"got `{self.event}` from {payload['repository']['full_name']}"
 
 
